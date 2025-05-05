5f8f16639fd0428928566d8e4b743c0ddcadcaaa
Luciano S Dri <luciano.s.dri@gmail.com>
2025-05-05 12:07:08 -0300

    break everything

---
+++ firstScript.ts
@@ -0,0 +1,10 @@
+import * as hz from 'horizon/core';
+
+class firstScript extends hz.Component<typeof firstScript> {
+  static propsDefinition = {};
+
+  start() {
+
+  }
+}
+hz.Component.register(firstScript);
